dec0ac950d116b2b62d2007554f57daf8bd1d163
Raphael Alampay <raphael.alampay@gmail.com>
2014-12-06 12:13:24 +0800

    Save selected patch image in directory specified

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 #include <ctime>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <opencv2/opencv.hpp>
+
+#define LINE_COLOR Scalar(0, 0, 255)
+#define LINE_THICKNESS 5
 
 using namespace std;
 using namespace cv;
@@ -15,10 +20,32 @@ int patch_width;
 int patch_height;
 int image_width;
 int image_height;
+const char *directory;
 
 void syntax()
 {
-  printf("patchselector [image] [w] [h]\n");
+  printf("patchselector [image] [w] [h] [dir]\n");
+}
+
+bool dirExists(const char *dir_path) 
+{
+  struct stat info;
+
+  if(stat(dir_path, &info) != 0)
+  {
+    printf("Cannot access %s\n", dir_path);
+    return false;
+  } 
+  else if(info.st_mode & S_IFDIR)
+  {
+    printf("%s is a directory\n", dir_path); 
+    return true;
+  }
+  else
+  {
+    printf("%s is not a directory\n", dir_path); 
+    return false;
+  }
 }
 
 void onmouse(int event, int x, int y, int flags, void *params)
@@ -36,15 +63,14 @@ void onmouse(int event, int x, int y, int flags, void *params)
   Point end_point(end_x, end_y);
 
   //printf("Drawing rectangle with start_point(%d, %d) and end_point(%d, %d)\n", start_x, start_y, end_x, end_y);
-  rectangle(buffer_image, start_point, end_point, Scalar(0, 0, 255));
+  rectangle(buffer_image, start_point, end_point, LINE_COLOR, LINE_THICKNESS);
 
   // draw rectangle on buffer image and display on window
   imshow(windowName, buffer_image);
 
   // timestamp generation for image filename
   std::time_t now = std::time(NULL);
-  std::tm *ptm = std::localtime(&now);
-  char buffer[32];
+  std::stringstream ss;
 
   switch(event)
   {
@@ -54,18 +80,32 @@ void onmouse(int event, int x, int y, int flags, void *params)
       break;
     case CV_EVENT_LBUTTONUP:
       printf("Selecting patch at start_point(%d, %d) and end_point(%d, %d)\n", start_x, start_y, end_x, end_y);
-      break;
 
+      // Save file to directory
+      ss << directory << "/" << now << ".jpg";
+      std::string filename = ss.str();
+      printf("Saving filename: %s\n", ss.str().c_str());
+      imwrite(ss.str().c_str(), main_image(Rect(start_x, start_y, patch_width, patch_height)));
+      break;
   }
 }
 
 
 int main(int argc, char **argv)
 { 
-  if(argc != 4)
+  if(argc != 5)
   {
     syntax();
     exit(-1);
+  }
+
+  if(!dirExists(argv[4])) 
+  {
+    exit(-1);
+  }
+  else 
+  {
+    directory = argv[4];
   }
 
   main_image = imread(argv[1]);
